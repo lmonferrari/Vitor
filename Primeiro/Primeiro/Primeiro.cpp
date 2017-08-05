9efe8415204b6fe0f85fe074b7bf3cb2c7b9b6c3
lmonferrari <lmonferrari@outlook.com>
2017-08-04 21:04:49 -0300

    prototipo

--- Primeiro/Primeiro/Primeiro.cpp
+++ Primeiro/Primeiro/Primeiro.cpp
@@ -8,6 +8,84 @@
 
 using namespace std;
 
+//prototipos
+void gauss(float A_aumentada[LINHA][COLUNA_EXTENDIDA], float valores[LINHA]);
+void imprime_valores(float valores[LINHA]);
+void imprime_vetor(float vetor[LINHA]);
+void imprimi_matriz_aumentada(float A_aumentada[LINHA][COLUNA_EXTENDIDA]);
+void imprimi_matriz(float A_coeficientes[LINHA][COLUNA]);
+void cria_matriz_aumentada(float A_coeficientes[LINHA][COLUNA], float A_resultados[LINHA], float A_aumentada[LINHA][COLUNA_EXTENDIDA]);
+
+
+int main(void)
+{
+	cout.precision(4);
+	cout << fixed;
+
+	cout << "#####################################\n";
+	cout << "#                                   #\n";
+	cout << "# TRABALHO FEITO POR VITOR MONTEIRO # \n#    Objetivo: Calculo de trelica   #\n";
+	cout << "#                                   #\n";
+	cout << "#####################################\n\n";
+
+	float P1, P2 = 0; // pesos - valores dados pelo usuário
+	float *PP1, *PP2; // ponteiros para os pesos
+	float valores[TAMANHO]; // cb cd de db be ba
+	char pause;
+
+	cout << "[+] De o valor para Peso 1." << endl;
+	cin >> P1;
+	cout << "[+] De o valor para Peso 2." << endl;
+	cin >> P2;
+
+	// utilizando ponteiros
+	PP1 = &P1;
+	PP2 = &P2;
+
+	cout << "\n[+]Pesos selecionados " << "\n   [-]P1: " << *PP1 << "\n   [-]P2: " << *PP2 << endl;
+	cout << "\n[+]Endereco de memoria do Pesos selecionados " << "\n   [-]P1: " << PP1 << "\n   [-]P2: " << PP2 << endl;
+	cin >> pause;
+
+	float A_coeficientes[LINHA][COLUNA] =
+	{
+		0.5000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000 ,
+		-0.86602540378, -1.0000, 0.0000, 0.0000, 0.0000, 0.0000,
+		0.0000, 1.0000, -1.0000, 0.0000, 0.0000, 0.00000,
+		0.0000, 0.0000, 0.0000, 1.0000, 0.0000, 0.0000,
+		0.0000, 0.0000, 0.0000, -0.86602540378,-0.86602540378, 0.0000,
+		1.0000, 0.0000, 0.0000, 0.5000, -0.5000, -1.0000
+	};
+	float A_resultados[LINHA] =
+	{
+		P2, 0, 0, P1, 0, 0,
+	};
+	float A_aumentada[LINHA][COLUNA_EXTENDIDA];
+
+	cout << "\n[+]Matriz de coeficientes" << endl << endl;
+	imprimi_matriz(A_coeficientes);
+	cin >> pause;
+
+	cout << "\n[+]Vetor de resultados" << endl << endl;
+	imprime_vetor(A_resultados);
+	cin >> pause;
+
+	cout << "\n[+]Criando matriz aumentada para usar metodo de Gauss\n\n";
+	cria_matriz_aumentada(A_coeficientes, A_resultados, A_aumentada);
+	imprimi_matriz_aumentada(A_aumentada);
+	cin >> pause;
+
+	cout << "\n[+]Matriz apos Algoritmo de Gauss\n" << endl;
+	gauss(A_aumentada, valores);
+	imprimi_matriz_aumentada(A_aumentada);
+	cin >> pause;
+
+	cout << "\n[+]Imprimindo valores dos nos da trelica: \n" << endl;
+	imprime_valores(valores);
+	cin >> pause;
+
+	return 0;
+}
+
 //implementação do algoritmo de Gauss
 void gauss(float A_aumentada[LINHA][COLUNA_EXTENDIDA], float valores[LINHA]) {
 
@@ -43,7 +121,7 @@ void gauss(float A_aumentada[LINHA][COLUNA_EXTENDIDA], float valores[LINHA]) {
 }
 
 void imprime_valores(float valores[LINHA]) {
-	
+
 	for (int i = 0; i < LINHA; i++)
 	{
 		if (valores[i] < 0) {
@@ -51,13 +129,13 @@ void imprime_valores(float valores[LINHA]) {
 		}
 		else {
 			cout << "valores de x" << i + 1 << "  " << valores[i] << " Tracao" << endl;
-		}	
+		}
 	}
 	cout << endl;
 }
 
 void imprime_vetor(float vetor[LINHA]) {
-	
+
 	for (int i = 0; i < LINHA; i++)
 	{
 		cout << "Equacao " << i + 1 << ": " << vetor[i] << " " << endl;
@@ -109,73 +187,3 @@ void cria_matriz_aumentada(float A_coeficientes[LINHA][COLUNA], float A_resultad
 		}
 	}
 }
-
-int main(void)
-{
-	cout.precision(4);
-	cout << fixed; 
-
-	cout << "#####################################\n";
-	cout << "#                                   #\n";
-	cout << "# TRABALHO FEITO POR VITOR MONTEIRO # \n#    Objetivo: Calculo de trelica   #\n";
-	cout << "#                                   #\n";
-	cout << "#####################################\n\n";
-
-	float P1, P2 = 0; // pesos - valores dados pelo usuário
-	float *PP1, *PP2; // ponteiros para os pesos
-	float valores[TAMANHO]; // cb cd de db be ba
-	char pause;
-
-	cout << "[+] De o valor para Peso 1." << endl;
-	cin >> P1;
-	cout << "[+] De o valor para Peso 2." << endl;
-	cin >> P2;
-	
-	// utilizando ponteiros
-	PP1 = &P1;
-	PP2 = &P2;
-
-	cout << "\n[+]Pesos selecionados " << "\n   [-]P1: " << *PP1 << "\n   [-]P2: " << *PP2 << endl;
-	cout << "\n[+]Endereco de memoria do Pesos selecionados " << "\n   [-]P1: " << PP1 << "\n   [-]P2: " << PP2 << endl;
-	cin >> pause;
-
-	float A_coeficientes[LINHA][COLUNA] =
-	{
-		0.5000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000 ,
-		-0.86602540378, -1.0000, 0.0000, 0.0000, 0.0000, 0.0000,
-		0.0000, 1.0000, -1.0000, 0.0000, 0.0000, 0.00000,
-		0.0000, 0.0000, 0.0000, 1.0000, 0.0000, 0.0000,
-		0.0000, 0.0000, 0.0000, -0.86602540378,-0.86602540378, 0.0000,
-		1.0000, 0.0000, 0.0000, 0.5000, -0.5000, -1.0000
-	};
-	float A_resultados[LINHA] =
-	{
-		P2, 0, 0, P1, 0, 0,
-	};
-	float A_aumentada[LINHA][COLUNA_EXTENDIDA];
-
-	cout << "\n[+]Matriz de coeficientes" << endl << endl;
-	imprimi_matriz(A_coeficientes);
-	cin >> pause;
-	
-	cout << "\n[+]Vetor de resultados" << endl << endl;
-	imprime_vetor(A_resultados);
-	cin >> pause;
-	
-	cout << "\n[+]Criando matriz aumentada para usar metodo de Gauss\n\n";
-	cria_matriz_aumentada(A_coeficientes, A_resultados, A_aumentada);
-	imprimi_matriz_aumentada(A_aumentada);
-	cin >> pause;
-	
-	cout << "\n[+]Matriz apos Algoritmo de Gauss\n" << endl;
-	gauss(A_aumentada, valores);
-	imprimi_matriz_aumentada(A_aumentada);
-	cin >> pause;
-	
-	cout << "\n[+]Imprimindo valores dos nos da trelica: \n" << endl;
-	imprime_valores(valores);
-	cin >> pause;
-
-	return 0;
-}
-
